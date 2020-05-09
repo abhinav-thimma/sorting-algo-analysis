3a7b36f73f8620f9ccbfb6f82cfa2a763b32fe54
tabhi0797 <abhinav071197@gmail.com>
2020-05-09 22:52:28 +0530

    QuickSort: added a iterative version of quick sort to prevent recursion depth issues

--- quick_sort.py
+++ quick_sort.py
@@ -4,7 +4,11 @@ sys.setrecursionlimit(1500)
 
 class Sort:
     array_access_count = 0
-    def quick_sort(self, nums):
+    '''
+    recursive version of quick sort algorithm
+    '''
+    def quick_sort_recursive(self, nums):
+        self.array_access_count = 0
         return self.sort(nums, 0, len(nums) - 1)
 
     def sort(self, nums, start, end):
@@ -16,6 +20,32 @@ class Sort:
             self.sort(nums, start, pivotPosition - 1)
             self.sort(nums, pivotPosition +1, end)
 
+        return nums, self.array_access_count
+
+    '''
+    iterative version of quick-sort which solves recursion depth issues
+    '''
+    def quick_sort_iterative(self, nums):
+        self.array_access_count = 0
+        stack = [] 
+        stack.append(0)
+        stack.append(len(nums) - 1)
+        top = 1
+
+        while(len(stack) > 1):
+            end = stack.pop()
+            start = stack.pop()
+
+            # partition the array based on these values
+            pivotPosition = self.partition(nums, start, end)
+
+            if(pivotPosition - 1 > start):
+                stack.append(start)
+                stack.append(pivotPosition - 1)
+            
+            if(pivotPosition + 1 < end):
+                stack.append(pivotPosition + 1)
+                stack.append(end)
         return nums, self.array_access_count
 
     '''
