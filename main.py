4b5f1c48b55a459420ad21872b97b4bd445eb59b
tabhi0797 <abhinav071197@gmail.com>
2020-05-13 23:20:59 +0530

    Sorts: restructured the sorting algorithms ArrayGenerator: added a class to generate required arrays for sorting Main: implemented a method to test for different types of arrays

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from bubble_sort import Sort  as BubbleSort
-from insertion_sort import Sort  as InsertionSort
-from selection_sort import Sort  as SelectionSort
-from radix_sort import Sort  as RadixSort
-from merge_sort import Sort  as MergeSort
-from quick_sort import Sort  as QuickSort
-from cocktail_sort import Sort as CocktailSort
-from gnome_sort import Sort as GnomeSort
-from shell_sort import Sort as ShellSort
+from array_generator import ArrayGenerator
 
-import numpy as np 
+from sorting_algorithms.bubble_sort import Sort  as BubbleSort
+from sorting_algorithms.insertion_sort import Sort  as InsertionSort
+from sorting_algorithms.selection_sort import Sort  as SelectionSort
+from sorting_algorithms.radix_sort import Sort  as RadixSort
+from sorting_algorithms.merge_sort import Sort  as MergeSort
+from sorting_algorithms.quick_sort import Sort  as QuickSort
+from sorting_algorithms.cocktail_sort import Sort as CocktailSort
+from sorting_algorithms.gnome_sort import Sort as GnomeSort
+from sorting_algorithms.shell_sort import Sort as ShellSort
+
 import time
 
 def check_sorting(nums):
@@ -17,40 +18,6 @@ def check_sorting(nums):
             print('NOT_SORTED')
             return None
     return nums
-
-class ArrayGenerator:
-    def __init__(self, min = -10000, max = 10000):
-        self.min = min
-        self.max = max
-        
-    '''
-    generates arrays with random unordered elements
-    '''
-    def get_random_arrays(self, no_of_arrays, size_of_array):
-        arrays = []
-        for i in range(no_of_arrays):
-            nums = [np.random.randint(self.min, self.max) for i in range(size_of_array)]
-            arrays.append(nums)
-        return arrays
-
-    '''
-    generates arrays with sorted random elements
-    '''
-    def get_random_sorted_arrays(self, no_of_arrays, size_of_array):
-        arrays = []
-        for i in range(no_of_arrays):
-            nums = [np.random.randint(self.min, self.min + np.random.randint(0, 50))]
-            for pos in range(size_of_array - 1):
-                nums.append(np.random.randint(nums[pos], nums[pos] + np.random.randint(0, 50) + 1))
-            arrays.append(nums)
-        return arrays
-
-    def get_linear_arrays(self, no_of_arrays, size_of_arrays):
-        arrays = []
-        for i in range(no_of_arrays):
-            nums = [num for num in range(0, size_of_arrays)]
-            arrays.append(nums)
-        return arrays
 
 def compare_sorts(sort_function1, sort_function2):
     arrays = ArrayGenerator().get_random_arrays(1000, 1000)
@@ -79,7 +46,7 @@ def time_sort_algorithms(sort_map):
         t1 = time.process_time()
         print(f'Time for {name}\t: {str(t1 - t)}\t  Array access count: {str(access_count)}')
 
-def test_sort_algorithm(sort_function, steps):
+def test_sort_algorithm_raw(sort_function, steps):
     for i in range(steps):
         t = time.process_time()
         array = ArrayGenerator().get_random_arrays(1, 100*(10**i))[0]
@@ -87,6 +54,19 @@ def test_sort_algorithm(sort_function, steps):
         check_sorting(sorted_array)
         t1 = time.process_time()
         print(f'Array size: {len(array)} Array access count: {str(access_count)}  time taken: {str(t1 - t)}')
+
+def test_sort_algorithm_with_generated_arrays(sort_function, array_gen_map):
+    for name, array_generator in array_gen_map.items():
+        arrays = array_generator(10, 1000)
+        t = time.process_time()
+        access_count = 0
+        for arr in arrays:
+            sorted_arr, count = sort_function(arr.copy())
+            access_count += count
+            check_sorting(sorted_arr)
+        t1 = time.process_time()
+        print(f'Time for {name}\t: {str(t1 - t)}\t  Array access count: {str(access_count)}')
+        
 
 sort_map = {}
 sort_map['selection_sort'] = SelectionSort().selection_sort
@@ -100,5 +80,11 @@ sort_map['quick_sort_iterative'] = QuickSort().quick_sort_iterative
 sort_map['gnome_sort'] = GnomeSort().gnome_sort
 sort_map['shell_sort'] = ShellSort().shell_sort
 
-time_sort_algorithms(sort_map)
-# test_sort_algorithm(QuickSort().quick_sort, 5)
+array_gen_map = {}
+array_gen_map['random'] = ArrayGenerator().get_random_arrays
+array_gen_map['random_sorted'] = ArrayGenerator().get_random_sorted_arrays
+array_gen_map['linear'] = ArrayGenerator().get_linear_arrays
+
+# time_sort_algorithms(sort_map)
+# test_sort_algorithm_raw(BubbleSort().bubble_sort, 2)
+# test_sort_algorithm_with_generated_arrays(BubbleSort().bubble_sort, array_gen_map)
