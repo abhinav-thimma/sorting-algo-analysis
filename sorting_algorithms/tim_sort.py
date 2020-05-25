025ceba5ae7afb8b8053469c9844cc07a8720934
tabhi0797 <abhinav071197@gmail.com>
2020-05-25 21:54:14 +0530

    TimSort: added in place merge of sorted arrays

--- sorting_algorithms/tim_sort.py
+++ sorting_algorithms/tim_sort.py
@@ -16,7 +16,7 @@ class Sort:
                 nums[j + 1] = curr
                 self.array_access_count+=1
     
-    def merge_in_place(self, nums, left, mid, right):
+    def merge(self, nums, left, mid, right):
         left_arr = nums[left:mid]
         right_arr = nums[mid:right+1]
         self.array_access_count+=(right - left + 1)
@@ -41,6 +41,35 @@ class Sort:
 
         nums[left:right+1] = merged_arr
 
+    '''
+    memory efficient merge of sorted arrays
+    '''
+    def merge_in_place(self, nums, left, mid, right):
+        left_size = mid - left
+        right_size = right - mid
+
+        left_arr = nums[left:mid].copy()
+        l_idx, m_idx, r_idx = 0, left, mid
+
+        while((l_idx < len(left_arr)) and (mid <= r_idx <= right)):
+            if(left_arr[l_idx] <= nums[r_idx]):
+                nums[m_idx] = left_arr[l_idx]
+                m_idx+=1
+                l_idx+=1
+            else:
+                nums[m_idx] = nums[r_idx]
+                m_idx+=1
+                r_idx+=1
+        
+        while(l_idx < len(left_arr)):
+            nums[m_idx] = left_arr[l_idx]
+            m_idx+=1
+            l_idx+=1
+        while(r_idx <= right):
+            nums[m_idx] = nums[r_idx]
+            r_idx+=1
+            m_idx+=1
+
     def tim_sort(self, nums):
         self.array_access_count = 0
         # sorting sub arrays of SPLIT_SIZE length
@@ -56,13 +85,3 @@ class Sort:
                 self.merge_in_place(nums, left, mid, right)
             batch_range*=2
         return nums, self.array_access_count
-
-# def check_sorting(nums):
-#     for i in range(1, len(nums)):
-#         if(nums[i-1] > nums[i]):
-#             print('NOT_SORTED')
-#             return None
-#     return nums
-
-# nums = [np.random.randint(-10000, 10000) for i in range(1000000)]
-# check_sorting(Sort().tim_sort(nums)[0])
