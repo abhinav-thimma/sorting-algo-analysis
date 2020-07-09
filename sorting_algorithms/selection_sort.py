00736b9c77a8d4e2305f57857bb79c589c9dfe6b
abhinav-thimma <abhinav071197@gmail.com>
2020-07-09 22:46:56 +0530

    SelectionSort: added the direction parameter for desc sorts

--- sorting_algorithms/selection_sort.py
+++ sorting_algorithms/selection_sort.py
@@ -1,11 +1,15 @@
 class Sort:
-    def selection_sort(self, nums):
+    def selection_sort(self, nums, direction = 'asc'):
+        if (direction == 'asc'):
+            comparator = lambda x, y: (x < y)  
+        else:
+            comparator = lambda x, y: (x > y)
         array_access_count = 0
         for i in range(0, len(nums) - 1):
             min, pos = nums[i], i
             # step: find min in the remaining array and replace with the current element
             for j in range(i + 1, len(nums)):
-                if(nums[j] < min):
+                if(comparator(nums[j], min)):
                     min, pos = nums[j], j
                     array_access_count += 1
                 array_access_count += 1
